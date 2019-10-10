558c54e52f6f9d2ffa244e2a72cbca85059a4f58
Gnak <523748995@qq.com>
2019-10-10 10:41:42 +0800

    捕捉异常

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 const config  = require('./config')
-const app = new Koa();
 const Router = require('koa-router')
+const bodyparser =  require('koa-bodyparser')
 const requireDir = require('require-directory')
+const catchEror = require('./middlewares/exception')
 const apiDir = `${process.cwd()}/api`
 
+const app = new Koa();
+app.use(catchEror)
+app.use(bodyparser)
 // 批量引入文件
 requireDir(module, apiDir, {
     visit: (obj) => {
